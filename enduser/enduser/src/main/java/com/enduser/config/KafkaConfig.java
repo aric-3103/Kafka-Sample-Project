60d1078e3cd1b9509233bd256ad9aab4f60bfec5
arpitshukla0331 <3P-ashukla@apriori.com>
2023-09-28 20:40:56 +0530

    added basic kafka project

---
+++ enduser/enduser/src/main/java/com/enduser/config/KafkaConfig.java
@@ -0,0 +1,20 @@
+package com.enduser.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.KafkaListener;
+
+@Configuration
+public class KafkaConfig {
+
+    private Logger logger = LoggerFactory.getLogger(KafkaConfig.class);
+
+    @KafkaListener(topics = AppConsumerConstant.LOCATION_UPDATE_TOPIC, groupId = AppConsumerConstant.GROUP_ID)
+    public void updatedLocation(String value) {
+
+        System.out.println(value);
+
+
+    }
+}
